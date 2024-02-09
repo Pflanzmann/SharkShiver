7f68162b448a4b719821b5c2eacf40132532e3e6
Ronny Brzeski <Svenbrzeski3@freenet.de>
2024-02-09 20:15:07 +0100

    Add dochs for message receiver

--- src/main/java/com/shiver/components/ShiverMessageReceiver.java
+++ src/main/java/com/shiver/components/ShiverMessageReceiver.java
@@ -7,10 +7,20 @@ package com.shiver.components;
 public interface ShiverMessageReceiver {
 
     /**
-     * Function that gets called when a message for a group got received
+     * Function that gets called from the [SharkShiverComponent] when a message for a group got received
      *
      * @param groupId - id of the group the message got send with
      * @param content - the content of the message as byte array
      */
     void receiveShiverMessage(CharSequence groupId, byte[] content);
+
+    /**
+     * Function that gets called by the [SharkShiverComponent] when something went wrong
+     *
+     * @param groupId
+     * @param encryptedMessage
+     * @param exception
+     */
+    void errorReceivingMessage(CharSequence groupId, byte[] encryptedMessage, Exception exception);
+
 }
