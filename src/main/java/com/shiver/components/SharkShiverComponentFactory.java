ce63faa8ddcbe7f834aaf3151f5c5cccd7baf263
Ronny Brzeski <Svenbrzeski3@freenet.de>
2024-02-11 21:02:46 +0100

    Improve factory

--- src/main/java/com/shiver/components/SharkShiverComponentFactory.java
+++ src/main/java/com/shiver/components/SharkShiverComponentFactory.java
@@ -1,32 +1,21 @@
 package com.shiver.components;
 
+import com.shiver.ShiverGroupFactory;
 import com.shiver.ShiverGroupStorage;
 import com.shiver.ShiverMediator;
 import com.shiver.ShiverSecurity;
-import com.shiver.ShiverGroupFactory;
 import net.sharksystem.SharkComponent;
 import net.sharksystem.SharkComponentFactory;
 
 public class SharkShiverComponentFactory implements SharkComponentFactory {
-    private ShiverGroupStorage shiverGroupStorage = null;
-    private ShiverSecurity shiverSecurity = null;
-    private ShiverGroupFactory groupFactory = null;
-    private ShiverMediator shiverMediator = null;
-    private SharkShiverComponentImpl instance = null;
+    private final SharkShiverComponentImpl instance;
 
     public SharkShiverComponentFactory(ShiverSecurity shiverSecurity, ShiverGroupStorage shiverGroupStorage, ShiverGroupFactory groupFactory, ShiverMediator shiverMediator) {
-        this.shiverSecurity = shiverSecurity;
-        this.shiverGroupStorage = shiverGroupStorage;
-        this.groupFactory = groupFactory;
-        this.shiverMediator = shiverMediator;
+        instance = new SharkShiverComponentImpl(shiverSecurity, shiverGroupStorage, groupFactory, shiverMediator);
     }
 
     @Override
     public SharkComponent getComponent() {
-        if (instance == null) {
-            return new SharkShiverComponentImpl(shiverSecurity, shiverGroupStorage, groupFactory, shiverMediator);
-        } else {
-            return instance;
-        }
+        return instance;
     }
 }
